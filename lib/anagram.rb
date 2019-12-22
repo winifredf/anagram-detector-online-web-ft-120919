f541babebb30c67e1dff32e6be7d77099675d75b
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 12:59:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(word_array)
     word_array.select do |element|
-      (@word.split("").sort == @word.split("")
+      (@word.split("").sort == (element.split("").sort)
     end
   end
 end
